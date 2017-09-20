b4e321961f4d655b047144bc9d4dc001ee4a7984
nitslaszlo@gmail.com <nitslaszlo@gmail.com>
2017-09-20 05:44:22 +0200

    Upload

---
+++ ts/app.ts
@@ -0,0 +1,10 @@
+﻿import * as http from "http";
+import { Content } from "./Content";
+
+class Program {
+    constructor() {
+        http.createServer(new Content().Content).listen(8080);
+    }
+}
+
+new Program();
