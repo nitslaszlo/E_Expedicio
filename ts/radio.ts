e6011c8124d3703b3307f40b2a288488e52a7bd5
bencefazekas <fazekas.bence@jedlik.helyi>
2017-09-28 11:04:32 +0200

    radio.ts frissités

--- ts/radio.ts
+++ ts/radio.ts
@@ -8,7 +8,9 @@ export class Radio {
       return valasz;
    }
 
-
+   static Main(): void {
+      const v: Vétel[] = [];
+   }
 
 
 }
@@ -19,15 +21,16 @@ export class Radio {
 
 
 
+// static void Main()
+// {
+  // List < Vétel > v = new List<Vétel>();
+//   using(StreamReader sr = new StreamReader("veetel.txt"))
+//   {
+//      while (!sr.EndOfStream)
+ //        v.Add(new Vétel(sr.ReadLine().Split(), sr.ReadLine()));
+ //  }
 
-//   class Radio
- //   {
- //       static bool szame(string szo)
- //       {
- //            bool valasz = true;
- //           for (int i = 0; i < szo.Length; i++)
- //           {
- //               if (szo[i] < '0' || szo[i] > '9') valasz = false;
- //           }
- //           return valasz;
- //       }
+ //  Console.WriteLine
+// ("2. feladat:\nAz első üzenet rögzítője:{0}\nAz utolsó üzenet rögzítője:{1}", v[0].R, v.Last().R);
+
+//   Console.Write("3. feladat:");
