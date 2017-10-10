9464488c6a0adaf3a8827de608938b5ba0b96ac9
kaldykristof@gmail.com <kaldy.kristof@jedlik.helyi>
2017-10-10 11:48:57 +0200

    Szépítés

--- ts/radio.ts
+++ ts/radio.ts
@@ -18,26 +18,26 @@ export class Radio {
       const fs: any = require("fs");
       const sorok: any = fs.readFileSync("veetel.txt").toString().split("\r\n");
       while (!fs.EndOfStream) v.push(new Vetel(fs.ReadLine().split(" "), fs.ReadLine()));
+      
+      // 2. feladat 
       console.log("2. feladat: \n Az első üzemet rögzítője: " + v[0].R + "\n Az utolsó üzenet rögzítője: " + v[length - 1].R);
 
+      //3. feladat 
       console.log("3. feladat:");
       console.log(v.Where(x => x.Ü.Contains("farkas")).Aggregate("", (c, n) => c += string.Format("\n" + n.N + ". nap " + n.R + ". rádióamatőr")));
       
-    //4. feladat.
+      // 4. feladat
       console.log("4. feladat:");
        
       var ki: string[];
-     // ki.push('elem'); - Hozzáadás listához.
+      // ki.push('elem'); - Hozzáadás listához.
       
-     import fs = require('fs');
-     fs.readFileSync('adaas.txt', 'utf8'); 
+      import fs = require('fs');
+      fs.readFileSync('adaas.txt', 'utf8'); 
       
-     //7. feladat:
-     console.log("7. feladat:");
-     console.log("Adja meg a nap sorszámát!");
-     var sszN: number; 
+      // 7. feladat
+      console.log("7. feladat:");
+      console.log("Adja meg a nap sorszámát!");
+      var sszN: number; 
    }
-
 }
-
-
