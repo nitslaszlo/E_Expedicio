3efb3d6db00554ea87d33893dee0a53660623182
kaldykristof@gmail.com <kaldy.kristof@jedlik.helyi>
2017-10-12 09:48:53 +0200

    szépítés

--- ts/content.ts
+++ ts/content.ts
@@ -1,13 +1,15 @@
 ﻿import * as http from "http";
-import * as url from "url"; // űrlapokhoz, input kiolvasás
-import * as fs from "fs"; // file-kezelés
+import * as url from "url";
+import * as fs from "fs";
 import { Vetel } from "./vetel";
 
 export class Content {
 
     Content(req: http.ServerRequest, res: http.ServerResponse): void {
         res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-        res.write("<p>Hello TypeScript!</p>");
+        res.write("<form style='font-size:16px; background: LightGray'>");
+        res.write("<pre style='font-family: Courier'>");
+
         const v: Vetel[] = [];
         const sorok: string[] = fs.readFileSync("veetel.txt").toString().split("\r\n");
         let nap: number;
@@ -28,23 +30,23 @@ export class Content {
         res.write("2. Feladat:<br>");
         res.write(`Első üzenetet rögzítette: ${v[0].radios}<br>Utolsó üzenetet rögzítette: ${v[v.length - 1].radios}`);
 
-        res.write("<br>3. Feladat:<br>");
-        //const keres = /apples/gi; 
+        res.write("<br>\n3. Feladat:<br>");
         v.forEach((i) => {
             if (i.uzenet.search(/farkas/gi) != -1) {
                 res.write(i.nap + ". nap " + i.radios + ". rádióamatőr<br>");
             }
         });
 
-        res.write("4. Feladat:<br>");
+        res.write("\n4. Feladat:<br>");
         let napok: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
         v.forEach((i) => {
             napok[i.nap - 1]++;
         });
         for (let i = 0; i < 11; i++) {
-            res.write((i + 1) + ".nap: " + napok[i] + " rádióamatőr<br>");
+            res.write((i + 1) + ". nap: " + napok[i] + " rádióamatőr<br>");
         }
 
+        res.write("</pre></form>");
         res.end();
     }
 }
