b8f767b92b76cb2b44ae5d52088c00c7932a6134
kaldykristof@gmail.com <kaldy.kristof@jedlik.helyi>
2017-10-12 09:45:12 +0200

    közös munka

--- ts/content.ts
+++ ts/content.ts
@@ -1,13 +1,49 @@
 ﻿import * as http from "http";
 import * as url from "url"; // űrlapokhoz, input kiolvasás
 import * as fs from "fs"; // file-kezelés
+import { Vetel } from "./vetel";
 
 export class Content {
 
     Content(req: http.ServerRequest, res: http.ServerResponse): void {
         res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
         res.write("<p>Hello TypeScript!</p>");
+        const v: Vetel[] = [];
+        const sorok: string[] = fs.readFileSync("veetel.txt").toString().split("\r\n");
+        let nap: number;
+        let radios: number;
+        let sor2: string;
+        sorok.forEach((i) => {
+            if (i.length > 0) {
+                if (i.length < 90) {
+                    const m = i.split(" ");
+                    nap = parseInt(m[0]);
+                    radios = parseInt(m[1]);
+                } else {
+                    v.push(new Vetel(nap, radios, i));
+                }
+            }
+        });
 
+        res.write("2. Feladat:<br>");
+        res.write(`Első üzenetet rögzítette: ${v[0].radios}<br>Utolsó üzenetet rögzítette: ${v[v.length - 1].radios}`);
+
+        res.write("<br>3. Feladat:<br>");
+        //const keres = /apples/gi; 
+        v.forEach((i) => {
+            if (i.uzenet.search(/farkas/gi) != -1) {
+                res.write(i.nap + ". nap " + i.radios + ". rádióamatőr<br>");
+            }
+        });
+
+        res.write("4. Feladat:<br>");
+        let napok: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        v.forEach((i) => {
+            napok[i.nap - 1]++;
+        });
+        for (let i = 0; i < 11; i++) {
+            res.write((i + 1) + ".nap: " + napok[i] + " rádióamatőr<br>");
+        }
 
         res.end();
     }
--- ts/vetel.ts
+++ ts/vetel.ts
@@ -1,39 +1,12 @@
-import { List } from 'linqts';
-
 export class Vetel {
-   N: number;
-   R: number;
-   Ü: string;
-
-   constructor(m: string[], ü: string) {
-      this.N = parseInt(m[0]);
-      this.R = parseInt(m[1]);
-      this.Ü = ü;
-   }
-
-   public static op_Addition(v1: Vetel, v2: Vetel) : Vetel
-   {
-      let s: string = "";
-      for (let i: number = 0; i < 90; i++){
-         let ch: string = "#";
-         if (v1.Ü[i] != "#") ch = v1.Ü[i];
-         else if (v2.Ü[i] != "#") ch = v2.Ü[i];
-         s += ch;
-      }
-      const m: string[] = ["0", "0"];
-      return new Vetel(m, s);
-   }
-
-   public get MegfigyeltEgyedek(): string {
-      let m: string[] = this.Ü.split(" ")[0].split("/");
-      if ((m.length !== 2)) return "Nincs információ";
-      try {
-         let x: string;
-         x = "A megfigyelt egyedek száma: " + ((m[0]) + (m[1]));
-         return x;
-      }
-      catch (Error) {
-         return "Nincs információ";
-      }
+   nap: number;
+   radios: number;
+   uzenet: string;
+   azon: string;
+   constructor(nap: number, radios: number, sor: string) {
+      this.azon = nap.toString() + radios.toString();
+      this.nap = nap;
+      this.radios = radios;
+      this.uzenet = sor;
    }
 }
